506511412f87f37d337ee945ff519dc441cc223c
durganp1 <durganp1@gmail.com>
2020-10-19 22:41:29 -0500

    added tests to contact

--- src/components/Nav/__test__/index.test.js
+++ src/components/Nav/__test__/index.test.js
@@ -13,6 +13,8 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -23,6 +25,8 @@ describe('Nav component', () => {
             categories = {categories}
             setCurrentCategory = {mockSetCurrentCategory}
             currentCategory = {mockCurrentCategory}
+            contactSelected = {mockContactSelected}
+            setContactSelected = {mockSetContactSelected}
          />);
     });
     //SNAPSHOT TEST
@@ -31,6 +35,8 @@ describe('Nav component', () => {
             categories = {categories}
             setCurrentCategory = {mockSetCurrentCategory}
             currentCategory = {mockCurrentCategory}
+            contactSelected = {mockContactSelected}
+            setContactSelected = {mockSetContactSelected}
              />);
         // ASSERT VALUE COMPARISON
         expect(asFragment()).toMatchSnapshot();
@@ -44,6 +50,8 @@ describe('emoji is visible', () => {
             categories = {categories}
             setCurrentCategory = {mockSetCurrentCategory}
             currentCategory = {mockCurrentCategory}
+            contactSelected = {mockContactSelected}
+            setContactSelected = {mockSetContactSelected}
              />);
         // ASSERT
         expect(getByLabelText('camera')).toHaveTextContent('📸');
@@ -57,6 +65,8 @@ describe('links are visible', () => {
             categories = { categories }
             setCurrentCategory = {mockSetCurrentCategory}
             currentCategory = {mockCurrentCategory}
+            contactSelected = {mockContactSelected}
+            setContactSelected = {mockSetContactSelected}
              />);
         // ASSERT
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
